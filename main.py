a3ac75d5f4b7c26607160ae93e0a306c6ed7888c
leyotuason <leonilatuason2@gmail.com>
2024-11-09 21:42:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -395,7 +395,7 @@ def dashboard():
 
             st.success("Budget allocations saved successfully!")
             
-        all_entries = st.session_state.existing_data.head(20)  
+        all_entries = st.session_state.existing_data 
         st.dataframe(all_entries, width=600)
 
 # Initialize session state for logged-in status
