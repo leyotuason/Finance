9bfc37ef9dccf4d5600b627c8f79a8b4d67ea5ea
leyotuason <leonilatuason2@gmail.com>
2025-07-17 18:32:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import datetime
 page_bg = """
 <style>
 [data-testid="stAppViewContainer"] {
-    background-image: url("https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80");
+    background-image: url("https://unsplash.com/photos/northern-lights-3l3RwQdHRHg");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
